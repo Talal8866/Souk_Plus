d6570c9de1e180e3974f5d9625c042a78c8fcea0
Ibrahimoniro <l.z9@hotmail.com>
2024-10-13 21:31:53 +0300

    Added .env file and backend folder structure

--- src/backend/server.js
+++ src/backend/server.js
@@ -1,24 +1,13 @@
+require('dotenv').config({ path: "./src/backend/config/.env" });
 const express = require('express');
-const mongoose = require('mongoose');
-const cors = require('cors');
-const bodyParser = require('body-parser');
+const connectDB = require('./config/db');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
-
-// Middleware
-app.use(cors())
-app.use(bodyParser.json());
+const PORT = process.env.PORT;
+const url = process.env.DB_URI;
 
 // Connect to MongoDB
-mongoose.connect('mongodb+srv://Ibra:1234@online-store-dev.d2u6t.mongodb.net/?retryWrites=true&w=majority&appName=ONLINE-STORE-DEV')
-.then(() => console.log('MongoDB connected'))
-.catch(err => console.log(err));
-
-// Sample Route
-app.get('/', (req, res) => {
-    res.send('Online Stores Server!');
-});
+connectDB();
 
 // Start the Server
 app.listen(PORT, () => {
